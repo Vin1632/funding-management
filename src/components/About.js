30f51a0489a4e67c3046d01341bf830d5d54b4d5
zahrakotwal <zahrakotwal@gmail.com>
2024-04-23 22:39:58 +0200

    Edit About files

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,15 @@ const About = () => {
 
   return (
     <div className="About">
-        <p>Hello</p>
+        <div class="about-us-box">
+          <h2>Welcome to FundDocker</h2> 
+          <p>At <em>FundDocker</em>, we're dedicated to facilitating meaningful connections between organizations and initiatives that drive positive change. 
+            With a focus on streamlining funding opportunities, we provide a centralized platform for organizations to showcase their support for educational, business, and event initiatives. 
+            Our goal is to empower organizations to make a lasting impact on society while efficiently managing their funding applications. 
+          </p>
+          <p><em>Sail Towards Success with FundDocker: Anchoring Dreams, Charting Futures!</em></p>
+
+        </div>
     </div>
   );
 };
