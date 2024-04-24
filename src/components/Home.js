7746730eae09f8f960c512ad984a0991d280245b
Thabiso Mahlaola <2426592@students.wits.ac.za>
2024-04-24 12:19:21 +0200

    update log/home?admin

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,11 @@ import "../styles/dashboard.css";
 import AdminDashboard from "./Admin-dashboard";
 import logo from '../assets/FundDocker_logo.jpg';
 import backgroundImage from '../assets/sea-background.jpg'
+import app from "../firebase.js";
+import { getDatabase, ref, set, push } from "firebase/database";
 
 const Home = () => {
-  const { logOut, user } = useUserAuth();
+
   const navigate = useNavigate();
   const [formSubmitted, setFormSubmitted] = useState(false); // State to track form submission
 
@@ -24,14 +26,6 @@ const Home = () => {
     }
   };
 
-  const handleLogout = async () => {
-    try {
-      await logOut();
-      navigate("/");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
 
   // Function to handle FormAction change
   const handleFormActionChange = (e) => {
@@ -56,10 +50,50 @@ const Home = () => {
   };
 
   // Function to handle navigation to new page upon form submission
-  const handleFormSubmission = () => {
+  //companies
+  let [CompanyNamename, set_Comname] = useState("");
+  let [RepName, set_RepName] = useState("");
+  let [NAAM, set_naam] = useState("");
+  let [surname, set_surname] = useState("");
+  const [permission, setPermission] = useState("unbloked");
+
+
+  const handleFormSubmission = async () => {
+    
+    const db = getDatabase(app);
+
+    const newDocRef = push(ref(db, "Fundmanagers"));
+    set(newDocRef, {
+      CompanyName: CompanyNamename,
+      Rep_Name: RepName,
+      business : (document.getElementById("BusinessCheckbox_C").checked ? "1" : "0"),
+      education : (document.getElementById("EducationCheckbox_C").checked ? "1" : "0"),
+      events : (document.getElementById("EventsCheckbox_C").checked ? "1" : "0"),
+      permission : permission
+    }).then( () => {
+      alert("data saved successfully")
+    }).catch((error) => {
+      alert("error: ", error.message);
+    })
+
+    const user_info = push(ref(db, "users"));
+    set(user_info, {
+      Name: NAAM,
+      Surname: surname,
+      Birthday : (document.getElementById("birthday").value), 
+      business : (document.getElementById("BusinessCheckbox").checked ? "1" : "0"),
+      education : (document.getElementById("EducationCheckbox").checked ? "1" : "0"),
+      events : (document.getElementById("EventsCheckbox").checked ? "1" : "0"),
+      permission : permission
+    }).then( () => {
+      alert("data saved successfully")
+    }).catch((error) => {
+      alert("error: ", error.message);
+    })
+    
     navigate("/AdminDashboard"); // Navigate to the desired page upon form submission
   };
-
+  
   return (
     <>
       <div className="floating-background-image">
@@ -92,43 +126,53 @@ const Home = () => {
 
           <div id="UserDetails" style={{display: 'none'}}>
             <label>Name:</label>
-            <input type="text" id="Name" name="Name" /><br /><br />
+            <input type="text" id="Name" name="Name" value={NAAM} 
+                onChange={(e) => set_naam(e.target.value)}  /><br /><br />
             <label>Surname:</label>
-            <input type="text" id="Surname" name="Surname" /><br /><br />
+            <input type="text" id="Surname" name="Surname" value={surname} 
+                onChange={(e) => set_surname(e.target.value)}  /><br /><br />
             <label>Date of Birth:</label>
             <input type="date" id="birthday" name="birthday" /><br /><br />
             <p><strong>Looking for Funding?</strong></p>
-            <label className="container_details">Education
-              <input type="checkbox" />
+            <label className="container_details" >Education
+              <input type="checkbox" id="EducationCheckbox" />
               <span className="checkmark"></span>
             </label>
-            <label className="container_details">Business
-              <input type="checkbox" />
+            <label className="container_details" >Business
+              <input type="checkbox" id="BusinessCheckbox" />
               <span className="checkmark"></span>
             </label>
             <label className="container_details">Events
-              <input type="checkbox" />
+              <input type="checkbox"  id="EventsCheckbox"/>
               <span className="checkmark"></span>
             </label>
             <input type="submit" value="Submit" onClick={handleFormSubmission} />
           </div>
 
+          {/* companies input */}
           <div id="FundingManagerDetails" style={{display: 'none'}}>
             <label>Company Name:</label>
-            <input type="text" id="CompanyName" name="CompanyName" /><br /><br />
+
+            <input type="text" id="CompanyName" name="CompanyName" value={CompanyNamename} 
+                onChange={(e) => set_Comname(e.target.value)} /><br /><br />
             <label>Representative Name:</label>
-            <input type="text" id="RepName" name="RepName" /><br /><br />
+
+
+            <input type="text" id="RepName" name="RepName" value={RepName} 
+                onChange={(e) => set_RepName(e.target.value)} /><br /><br />
             <p><strong>Which categories are you looking to fund?</strong></p>
-            <label className="container_details">Education
-              <input type="checkbox" checked="checked" />
+
+
+            <label className="container_details" >Education
+              <input type="checkbox" id="EducationCheckbox_C" />
               <span className="checkmark"></span>
             </label>
             <label className="container_details">Business
-              <input type="checkbox" />
+              <input type="checkbox"  id="BusinessCheckbox_C" />
               <span className="checkmark"></span>
             </label>
             <label className="container_details">Events
-              <input type="checkbox" />
+              <input type="checkbox" id="EventsCheckbox_C" />
               <span className="checkmark"></span>
             </label>
             <input type="submit" value="Submit" onClick={handleFormSubmission} />
@@ -139,21 +183,6 @@ const Home = () => {
         navigate("/AdminDashboard")
       )}
 
-      {/* {htmlContent && (
-        <div>
-          <iframe
-            title="HTML Content"
-            srcDoc={htmlContent}
-            style={{ width: "100%", height: "100vh", border: "none" }}
-          />
-        </div>
-      )} */}
-
-      {/* <div classNameName="d-grid gap-2">
-        <Button variant="primary" onClick={handleLogout}>
-          Log out
-        </Button>
-      </div> */}
     </>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
@@ -6,7 +6,8 @@ import GoogleButton from "react-google-button";
 import { useUserAuth } from "../context/UserAuthContext";
 import { Container, Row, Col } from "react-bootstrap";
 import "../styles/login.css";
-
+import app from "../firebase.js";
+import { getDatabase, ref, get } from "firebase/database";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -14,13 +15,38 @@ const Login = () => {
   const [error, setError] = useState("");
   const { logIn, googleSignIn } = useUserAuth();
   const navigate = useNavigate();
+  const [infoArray, setInfoArray] = useState([]);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const db = getDatabase(app);
+        const dbRef = ref(db, "Fundmanagers");
+        const snapshot = await get(dbRef);
+
+        if (snapshot.exists()) {
+          setInfoArray(Object.values(snapshot.val()));
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    checkUser();
+  }, []); // Run this effect only once after the component mounts
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
     try {
       await logIn(email, password);
-      navigate("/home");
+      
+      const foundVinJob = infoArray.some(item => item.Email === email);
+      if (foundVinJob) {
+        navigate("/AdminDashboard");
+      } else {
+        navigate("/home");
+      }
     } catch (err) {
       setError(err.message);
     }
@@ -38,52 +64,49 @@ const Login = () => {
 
   return (
     <>
-    <Container>
-    <Row>
-      <Col className="box2">
-      <div className="p-4 box">
-        <h2 className="mb-3">Funding Management Login</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
+      <Container>
+        <Row>
+          <Col className="box2">
+            <div className="p-4 box">
+              <h2 className="mb-3">Funding Management Login</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control
+                    type="email"
+                    placeholder="Email address"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
 
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Log In
-            </Button>
-          </div>
-        </Form>
-        <hr />
-        <div>
-          <GoogleButton
-            className="g-btn"
-            type="dark"
-            onClick={handleGoogleSignIn}
-          />
-        </div>
-        <div className="p-4 box mt-3 text-center">
-        Don't have an account? <Link to="/signup">Sign up</Link>
-      </div>
-      </div>
-
-
-
-      </Col>
-      </Row>
+                <div className="d-grid gap-2">
+                  <Button variant="primary" type="submit">
+                    Log In
+                  </Button>
+                </div>
+              </Form>
+              <hr />
+              <div>
+                <GoogleButton
+                  className="g-btn"
+                  type="dark"
+                  onClick={handleGoogleSignIn}
+                />
+              </div>
+              <div className="p-4 box mt-3 text-center">
+                Don't have an account? <Link to="/signup">Sign up</Link>
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </>
   );
