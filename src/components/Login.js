c68847b63e4b509c185cda6676a87bd6cfdcc9be
Thabiso Mahlaola <2426592@students.wits.ac.za>
2024-04-29 12:38:52 +0200

    user api

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
 
 
   const [email, setEmail] = useState("");
-  
+
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const { logIn, googleSignIn } = useUserAuth();
--- testSQL/get.js
+++ testSQL/get.js
@@ -11,28 +11,19 @@ const config = {
     },
     port: 1433
 }
-async function getMain(){
-    try{
+
+let email = "vin1632@gmail.com";
+async function getMain() {
+    try {
         let pool = await sql.connect(config);
-        // let result = await pool.request().query("UPDATE  [dbo].[User] SET Blocked = 1 WHERE role = 'Admin'");
-        // let res = await pool.request().query("insert into [dbo].[User](ID, role, Name, Surname, DateOfBirth, CompanyName, Email, Education, Business, Events, Blocked) values (13,'Admin', 'Peterson', 'polar', '2010-08-08', 'abc ltd', 'polar@gmail.com' , 1, 1, 0, 0)");
-        // let res1 = await pool.request().query("insert into [dbo].[User](ID, role, Name, Surname, DateOfBirth, CompanyName, Email, Education, Business, Events, Blocked) values (14,'Admin', 'Johns', 'bear', '2010-08-08', 'abc ltd', 'bear@gmail.com' , 1, 1, 0, 0)");
-        // let res2 = await pool.request().query("insert into [dbo].[User](ID, role, Name, Surname, DateOfBirth, CompanyName, Email, Education, Business, Events, Blocked) values (16,'Admin', 'Mike ', 'eats', '2010-08-08', 'abc ltd', 'ea563e_@gmail.com' , 1, 1, 0, 0)");
-        // let res3 = await pool.request().query("insert into [dbo].[User](ID, role, Name, Surname, DateOfBirth, CompanyName, Email, Education, Business, Events, Blocked) values (17,'Admin', 'Jake', 'people', '2010-08-08', 'abc ltd', 'white@gmail.com' , 1, 1, 0, 0)");
-        // let res4 = await pool.request().query("insert into [dbo].[User](ID, role, Name, Surname, DateOfBirth, CompanyName, Email, Education, Business, Events, Blocked) values (18,'Admin', 'Mclovin', 'at', '2010-08-08', 'abc ltd', 'bonnat@gmail.com' , 1, 1, 0, 0)");
-        // let res5 = await pool.request().query("insert into [dbo].[User](ID, role, Name, Surname, DateOfBirth, CompanyName, Email, Education, Business, Events, Blocked) values (19,'Admin', 'Jonah', 'night', '2010-08-08', 'abc ltd', 'kauo@gmail.com' , 1, 1, 0, 0)");
-        // let res6 = await pool.request().query("insert into [dbo].[User](ID, role, Name, Surname, DateOfBirth, CompanyName, Email, Education, Business, Events, Blocked) values (20,'Admin', 'guy', 'roll', '2010-08-08', 'abc ltd', 'me_nfu@gmail.com' , 1, 1, 0, 0)");
+        let res = await pool.request()
+            .input('email', sql.VarChar, email) // Bind the value of 'email' to the query
+            .query("INSERT INTO [dbo].[User] (Email) VALUES (@email)");
         
-        // let sqldata = await pool.request().query("ALTER TABLE [dbo].[User] ALTER COLUMN ID INT IDENTITY(1,1)");
-        let pop = await pool.request().query("insert into [dbo].[User](Email) VALUES ('THABS')");
-        let users = await pool.request().query("SELECT * from [dbo].[User]");
+        let users = await pool.request().query("SELECT * FROM [dbo].[User]");
         console.log(users.recordset);
         return users.recordset;
-
-        
-
-    }
-    catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
