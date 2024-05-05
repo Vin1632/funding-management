74e52db665fc332629536b29d0480bff6478cf37
unknown <anistplenitud@gmail.com>
2024-05-05 17:10:52 +0200

    approve or reject applications + fixes to first Form

---
+++ api/getApplicationsForManagersAds/index.js
@@ -0,0 +1,40 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const sql = require("mssql");
+
+    const userId = req.body.userId
+    
+    console.log(userId);
+
+    const config = {
+        user: "Amaan",
+        password: "P0p0p0p0p",
+        server: "fundingmanagement.database.windows.net",
+        database: "FundingManager",
+        options: {
+            encrypt: true,
+        },
+        port: 1433
+    }
+
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request()
+            .input('userId', sql.VarChar(255), userId)
+            .query('SELECT * \
+            FROM [dbo].[Applications] as app \
+            LEFT JOIN [dbo].Adverts as ads \
+            ON app.AdvertsID = ads.AdID \
+            Where ads.UserID = (SELECT ID FROM [dbo].[User] WhERE Email = @userId )');
+            context.res = {
+                status: 200,
+                body: result.recordset
+            };
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: { message: `Error deleting user: ${error.message}` }
+        };
+    }
+}
---
+++ api/updateApplicationStatus/index.js
@@ -0,0 +1,39 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const sql = require("mssql");
+
+    const status = req.body.status;
+    const id = req.body.id;
+
+    console.log(req.body);
+
+    const config = {
+        user: "Amaan",
+        password: "P0p0p0p0p",
+        server: "fundingmanagement.database.windows.net",
+        database: "FundingManager",
+        options: {
+            encrypt: true,
+        },
+        port: 1433
+    }
+
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request()
+            .input('status', sql.Bit, status)
+            .input('applicationId', sql.Int, id)
+            .query('UPDATE [dbo].[Applications] \
+            SET [AcceptedOrRejected] = @status WHERE Application_ID = @applicationId');
+            context.res = {
+                status: 200,
+                body: { message: result }
+            };
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: { message: `Error updating user: ${error.message}` }
+        };
+    }
+}
