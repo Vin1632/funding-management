980ffea9111c9f0cb3ff546a4da7470b0b6ca530
Amaan <hanslodamaan4@gmail.com>
2024-05-25 14:21:02 +0200

    testing

--- jest.config.js
+++ jest.config.js
@@ -11,13 +11,15 @@ module.exports = {
     "<rootDir>/src/components/Managers-dashboard/Manager-dashboard.js",
     "<rootDir>/src/components/Home.js",
     "<rootDir>/src/components/Users-dashboard/Users-dashboard.js",
+    "<rootDir>/src/context/Managers-dashboard/ManagersHome.js",
     "<rootDir>/src/context/UserAuthContext.js"
   ],
   coveragePathIgnorePatterns: [
     "<rootDir>/src/components/Managers-dashboard/Manager-dashboard.js",
     "<rootDir>/src/components/Home.js",
     "<rootDir>/src/components/Users-dashboard/Users-dashboard.js",
-    "<rootDir>/src/context/UserAuthContext.js"
+    "<rootDir>/src/context/UserAuthContext.js",
+    "<rootDir>/src/components/Managers-dashboard/ManagersHome.js"
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
 };
